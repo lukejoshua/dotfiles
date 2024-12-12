3f63e8da9e222fa68774b41281d31128c0dd4c60
Luke Joshua <28862223+lukejoshua@users.noreply.github.com>
2024-12-12 16:40:07 +0200

    Update .config/nvim/lua/plugins/core.lua

--- dot_config/nvim/lua/plugins/core.lua
+++ dot_config/nvim/lua/plugins/core.lua
@@ -113,13 +113,13 @@ return {
     "echasnovski/mini.surround",
     opts = {
       mappings = {
-        add = 'ys',     -- Add surrounding in Normal and Visual modes
-        delete = 'ds',  -- Delete surrounding
-        replace = 'cs', -- Replace surrounding
-        -- find = 'gzf', -- Find surrounding (to the right)
-        -- find_left = 'gzF', -- Find surrounding (to the left)
-        -- highlight = 'gzh', -- Highlight surrounding
-        -- update_n_lines = 'gzn', -- Update `n_lines`
+        add = 'ygs',            -- Add surrounding in Normal and Visual modes
+        delete = 'dgs',         -- Delete surrounding
+        replace = 'cgs',        -- Replace surrounding
+        find = 'gsf',           -- Find surrounding (to the right)
+        find_left = 'gsF',      -- Find surrounding (to the left)
+        highlight = 'gsh',      -- Highlight surrounding
+        update_n_lines = 'gsn', -- Update `n_lines`
       }
     }
   },
