e15c84f98db28eed2a19f1e9db3e2ed4a03f866f
Luke Joshua <28862223+lukejoshua@users.noreply.github.com>
2024-12-18 12:19:21 +0200

    Remove .config/fish/functions/fish_right_prompt.fish Update .config/fish/functions/fish_prompt.fish Add .config/fish/functions/fish_right_prompt.fish

--- dot_config/private_fish/functions/fish_prompt.fish
+++ dot_config/private_fish/functions/fish_prompt.fish
@@ -2,19 +2,3 @@
 function fish_prompt
     echo (set color normal) "❯ "
 end
-
-function fish_right_promt
-    set -l normal (set_color normal)
-
-    # Color the prompt differently when we're root
-    set -l color_cwd $fish_color_cwd
-    if functions -q fish_is_root_user; and fish_is_root_user
-        if set -q fish_color_cwd_root
-            set color_cwd $fish_color_cwd_root
-        end
-    end
-
-    echo -n -s (set_color $color_cwd) (prompt_pwd) $normal (fish_vcs_prompt) $normal
-
-
-end
