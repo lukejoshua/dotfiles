2c4eb188fd6f1bffadac3d9193e233c075408037
Luke Joshua <28862223+lukejoshua@users.noreply.github.com>
2024-12-28 14:01:22 +0200

    remove close buttons from nvim bufferline

--- dot_config/nvim/lua/plugins/ui.lua
+++ dot_config/nvim/lua/plugins/ui.lua
@@ -18,7 +18,7 @@ return {
     },
     {
         "nvim-neo-tree/neo-tree.nvim",
-        enabled = false
+        enabled = false,
     },
     {
         "akinsho/bufferline.nvim",
@@ -29,10 +29,11 @@ return {
                 max_name_length = 30,
                 numbers = "ordinal",
                 show_close_icon = false,
-                style_preset = require('bufferline').style_preset.minimal
+                show_buffer_close_icons = false,
+                style_preset = require("bufferline").style_preset.minimal,
             }
             return opts
-        end
+        end,
     },
     {
         "nvim-lualine/lualine.nvim",
@@ -43,13 +44,13 @@ return {
                         "mode",
                         ---@param mode string
                         fmt = function(mode)
-                            if string.find(mode, '-', nil, true) then
+                            if string.find(mode, "-", nil, true) then
                                 return mode:sub(1, 1) .. mode:sub(3, 3)
                             else
                                 return mode:sub(1, 1)
                             end
-                        end
-                    }
+                        end,
+                    },
                 },
                 lualine_b = {
                     LazyVim.lualine.root_dir(),
@@ -59,7 +60,7 @@ return {
                         "filename",
                         -- Filename and parent dir, with tilder as the home directory
                         path = 4,
-                        shorting_target = 40
+                        shorting_target = 40,
                     },
                     {
                         "diagnostics",
@@ -74,19 +75,35 @@ return {
                 lualine_x = {
                     Snacks.profiler.status(),
                     {
-                        function() return require("noice").api.status.mode.get() end,
-                        cond = function() return package.loaded["noice"] and require("noice").api.status.mode.has() end,
-                        color = function() return { fg = Snacks.util.color("Constant") } end,
+                        function()
+                            return require("noice").api.status.mode.get()
+                        end,
+                        cond = function()
+                            return package.loaded["noice"]
+                                and require("noice").api.status.mode.has()
+                        end,
+                        color = function()
+                            return { fg = Snacks.util.color("Constant") }
+                        end,
                     },
                     {
-                        function() return "  " .. require("dap").status() end,
-                        cond = function() return package.loaded["dap"] and require("dap").status() ~= "" end,
-                        color = function() return { fg = Snacks.util.color("Debug") } end,
+                        function()
+                            return "  " .. require("dap").status()
+                        end,
+                        cond = function()
+                            return package.loaded["dap"]
+                                and require("dap").status() ~= ""
+                        end,
+                        color = function()
+                            return { fg = Snacks.util.color("Debug") }
+                        end,
                     },
                     {
                         require("lazy.status").updates,
                         cond = require("lazy.status").has_updates,
-                        color = function() return { fg = Snacks.util.color("Special") } end,
+                        color = function()
+                            return { fg = Snacks.util.color("Special") }
+                        end,
                     },
                 },
                 lualine_y = {
@@ -108,20 +125,20 @@ return {
                             end
                         end,
                     },
-                    { "branch" }
+                    { "branch" },
                 },
                 lualine_z = {
                     {
-                        'location',
+                        "location",
                         fmt = function(str)
-                            return 'Column ' .. string.gsub(str, '^.+%:', '')
+                            return "Column " .. string.gsub(str, "^.+%:", "")
                         end,
                     },
                 },
             }
             opts.extensions = { "lazy" }
             return opts
-        end
+        end,
     },
 
     -- TODO: consider edgy.nvim for creating layouts
@@ -132,9 +149,9 @@ return {
     -------------------------------
 
     {
-        'stevearc/oil.nvim',
+        "stevearc/oil.nvim",
         config = function()
-            local oil = require('oil')
+            local oil = require("oil")
 
             oil.setup({
                 default_file_explorer = true,
@@ -144,16 +161,19 @@ return {
             vim.api.nvim_create_autocmd("User", {
                 pattern = "OilEnter",
                 callback = vim.schedule_wrap(function(args)
-                    if vim.api.nvim_get_current_buf() == args.data.buf and oil.get_cursor_entry() then
+                    if
+                        vim.api.nvim_get_current_buf() == args.data.buf
+                        and oil.get_cursor_entry()
+                    then
                         oil.open_preview({ split = "belowright" })
                     end
                 end),
             })
         end,
         keys = {
-            { "<C-n>", "<CMD>Oil<CR>", desc = "Open parent directory" }
+            { "<C-n>", "<CMD>Oil<CR>", desc = "Open parent directory" },
         },
         -- Optional dependencies
         dependencies = { { "echasnovski/mini.icons", opts = {} } },
-    }
+    },
 }
